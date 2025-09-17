fb8b14daa508059f41cf2876fd4889cd68bbe8f3
yasmincsun <yasmin.zubair.180@my.csun.edu>
2025-09-17 23:23:44 +0000

    basic login - no functions

--- src/components/loginPage/login.jsx
+++ src/components/loginPage/login.jsx
@@ -5,31 +5,34 @@ import emailIcon from '../assets/email.png'
 import passwordIcon from '../assets/password.png'
 
 const loginPage = () => {
+
+    const [action, setAction] = useState("Sign Up"); 
+
     return ( 
         <div className="container">
             <div className="header">
-                <div className="text">Sign Up</div>
+                <div className="text">{action}</div>
                 <div className="underline"></div>
             </div>
             <div className="inputs">
                 <div className="input">
-                    <img src={userIcon} alt="" />
+                    <img src={userIcon} width={40} height={40} alt="" />
                     <input type="text" placeholder='Name' />
                 </div>
                 <div className="input">
-                    <img src={emailIcon} alt="" />
+                    <img src={emailIcon} width={40} height={40} alt="" />
                     <input type="email" placeholder='Email'/>
                 </div>
                 <div className="input"> 
-                    <img src={passwordIcon} alt="" />
+                    <img src={passwordIcon} width={40} height={40} alt="" />
                     <input type="password" placeholder='Password'/>
                 </div>
             </div>
             <div className="forgotPassword">Forgot Password? <span>Click Here!</span></div>
 
             <div className="submit-container">
-                <div className="submit">Sign Up</div>
-                <div className="submit">Login</div>
+                <div className={action ==="Login"?"submit gray":"submit"}>Sign Up</div>
+                <div className={action ==="Sign Up"?"submit gray":"submit"}>Login</div>
 
             </div>
 
